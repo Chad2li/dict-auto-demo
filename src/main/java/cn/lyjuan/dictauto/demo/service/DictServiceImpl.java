dcc31d0428305366cc7aef1682052dd73f3fd3e5
Chad <li17206@163.com>
2022-08-25 00:02:24 +0800

    add IDictService.dict target parameter

--- src/main/java/cn/lyjuan/dictauto/demo/service/DictServiceImpl.java
+++ src/main/java/cn/lyjuan/dictauto/demo/service/DictServiceImpl.java
@@ -3,6 +3,7 @@ package cn.lyjuan.dictauto.demo.service;
 import cn.lyjuan.dictauto.demo.dto.DictItemDemoDto;
 import io.github.chad2li.dictauto.base.dto.DictItemDto;
 import io.github.chad2li.dictauto.base.service.IDictService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 /**
@@ -10,13 +11,15 @@ import org.springframework.stereotype.Service;
  * @date 2022/5/19 01:02
  * @since 1 create by chad
  */
+@Slf4j
 @Service(IDictService.SPRING_BEAN_NAME)
-public class DictServiceImpl implements IDictService<Long> {
+public class DictServiceImpl implements IDictService<Long, Object> {
     @Override
-    public DictItemDto<Long> dict(Long dictId, String type) {
+    public DictItemDto<Long> dict(Long dictId, String dictType, Object target) {
+        log.info("dict dictId: {}, dictType:{}, target:{}", dictId, dictType, target);
         DictItemDemoDto item = new DictItemDemoDto();
         item.setId(dictId);
-        item.setName(type);
+        item.setName(dictType);
         item.setRemark("remark11");
         return item;
     }
